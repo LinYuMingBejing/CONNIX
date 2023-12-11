a69043e216aca555c1c127d189443a299fd76c4b
jack-y-lin <jack_y_lin@trendmicro.com>
2023-12-11 18:02:27 +0800

    Setup a Sprint Boot project

---
+++ src/main/java/com/cinnox/linebot/CinnoxApplication.java
@@ -0,0 +1,13 @@
+package com.cinnox.linebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinnoxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinnoxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cinnox/linebot/CinnoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinnox.linebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinnoxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
